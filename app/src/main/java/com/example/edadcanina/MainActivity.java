41af65beb05dc848b930b1abe6c646a75506db93
fmora <fmora@utch.edu.mx>
2024-09-18 01:35:04 -0600

    primera parte

---
+++ app/src/main/java/com/example/edadcanina/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.edadcanina;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        EditText edadEdit = findViewById(R.id.edad_edit);
+        TextView resulTexto = findViewById(R.id.result_texto);
+        Button boton = findViewById(R.id.boton);
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String edad = edadEdit.getText().toString();
+
+                int edadInt = Integer.parseInt(edad);
+                int resultado = edadInt * 7;
+                String resultadoString = "La edad de un perro es de: " + resultado + " años";
+                resulTexto.setText(resultadoString);
+            }
+        });
+    }
+}
